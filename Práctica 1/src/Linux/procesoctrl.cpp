68106b6d321dd76d102b0f12459a687256f96248
Esteba Arango <marranoparael31@gmail.com>
2012-04-22 23:19:34 -0500

    Done, problema con los infinitos en guindos

--- Práctica 1/src/Linux/procesoctrl.cpp
+++ Práctica 1/src/Linux/procesoctrl.cpp
@@ -69,76 +69,82 @@ process getParamsToProcess(int argc, char *argv[]){
 
 void fCreateProcess(process *p){
 
-	int causa;
-	int retVal;
+    do{
+        
+    	int causa;
+    	int retVal;
 
-    //Pipes
-    int in[2];
-    int out[2];
-    int err[2];
-    
-    pid_t pid;
-    
-    pipe(in);
-    pipe(out);
-    pipe(err);
+        //Pipes
+        int in[2];
+        int out[2];
+        int err[2];
+        
+        pid_t pid;
+        
+        pipe(in);
+        pipe(out);
+        pipe(err);
 
-    if ( (pid = fork()) < 0 )
-    {
-        cerr << "FORK ERROR" << endl;
-        exit(0);
-    }
-    else  if (pid == 0)     // CHILD PROCESS
-    {
-        close(in[1]);
-        close(out[0]);
-
-        dup2(in[0], STDIN_FILENO);
-        close(in[0]);
-
-        dup2(out[1], STDOUT_FILENO);
-        close(out[1]);
-
-        dup2(err[1], STDERR_FILENO);
-        close(err[1]);
-
-        execl(p->path.c_str(), p->fileName.c_str(),(char *) 0);
-        // No se debe ejecutar este código
-        fprintf(stderr, "No pudo ejecutar procesoctrl %s\n", strerror(errno));
-    }
-    else        // PARENT PROCESS
-    {
-        int rv;
-        close(in[0]);
-        close(out[1]);
-        close(err[1]);
-        /*
-        pthread_t readingIn;
-        pthread_t readingErr;
-
-        pthread_create(&readingIn, NULL, &read, (void *) &out[0]);
-        pthread_join(readingIn,NULL);
-        pthread_create(&readingErr, NULL, &read, (void *) &err[0]);
-        pthread_join(readingErr,NULL);
-        */
-        wait(&retVal);
-
-
-        // Verifica si el hijo terminó bien
-        if (WIFEXITED(retVal)) {
-            causa = WEXITSTATUS(retVal);
+        if ( (pid = fork()) < 0 )
+        {
+            cerr << "FORK ERROR" << endl;
+            exit(0);
         }
-        else if (WIFSIGNALED(retVal)) { // Fue señalizado
-            causa = WTERMSIG(retVal);
+        else  if (pid == 0)     // CHILD PROCESS
+        {
+            close(in[1]);
+            close(out[0]);
+
+            dup2(in[0], STDIN_FILENO);
+            close(in[0]);
+
+            dup2(out[1], STDOUT_FILENO);
+            close(out[1]);
+
+            dup2(err[1], STDERR_FILENO);
+            close(err[1]);
+
+            execl(p->path.c_str(), p->fileName.c_str(),(char *) 0);
+            // No se debe ejecutar este código
+            fprintf(stderr, "No pudo ejecutar procesoctrl %s\n", strerror(errno));
         }
-        else if (WIFSTOPPED(retVal)) {
-            causa = WSTOPSIG(retVal);
-        }
-     }
-     cout<<"Proceso suicida "<<coloriar(VERDE,p->id)<<" termino por causa ";
-	 cout<<coloriar(ROJO,intToStr(causa))<<" -- Proceso Control "<<coloriar(AMARILLO,intToStr(p->control))<<", vidas restantes: "<<((--p->lives<0)?"infinitas":intToStr(p->lives))<<endl; 
+        else        // PARENT PROCESS
+        {
+            int rv;
+            close(in[0]);
+            close(out[1]);
+            close(err[1]);
+            /*
+            pthread_t readingIn;
+            pthread_t readingErr;
+
+            pthread_create(&readingIn, NULL, &read, (void *) &out[0]);
+            pthread_join(readingIn,NULL);
+            pthread_create(&readingErr, NULL, &read, (void *) &err[0]);
+            pthread_join(readingErr,NULL);
+            */
+            wait(&retVal);
+
+
+            // Verifica si el hijo terminó bien
+            if (WIFEXITED(retVal)) {
+                causa = WEXITSTATUS(retVal);
+            }
+            else if (WIFSIGNALED(retVal)) { // Fue señalizado
+                causa = WTERMSIG(retVal);
+            }
+            else if (WIFSTOPPED(retVal)) {
+                causa = WSTOPSIG(retVal);
+            }
+         }
+         cout<<"Proceso suicida "<<coloriar(VERDE,p->id)<<" termino por causa ";
+    	 cout<<coloriar(ROJO,intToStr(causa))<<" -- Proceso Control "<<coloriar(AMARILLO,intToStr(p->control))<<", vidas restantes: "<<((--p->lives<0)?"infinitas":intToStr(p->lives))<<endl; 
+    }while(p->lives != 0);
+     /*if(p->lives<0)
+            cout<<"Vidas vividas: "<<p->lives*-1;
+     cout<<endl;
     if(p->lives != 0)
-		fCreateProcess(p);
+		fCreateProcess(p);*/
 
 }
 
